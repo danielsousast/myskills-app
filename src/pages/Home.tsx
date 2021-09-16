6e1d7e454ae9c0306e3a0ea8223603da72d024f5
Daniel Sousa <daielsousa@Daniels-iMac-Pro.local>
2021-09-16 10:16:20 -0300

    feat: add icon and splash

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,130 +1,116 @@
-import React, { useState, useEffect } from 'react';
-import { 
-    View, 
-    Text, 
-    StyleSheet, 
-    TextInput, 
-    Platform,
-    FlatList,
-    Keyboard,    
-} from 'react-native';
+import React, { useState, useEffect } from "react";
+import SplashScreen from "react-native-splash-screen";
 
-import { Button } from '../components/Button';
-import { SkillCard } from '../components/SkillCard';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  FlatList,
+  Keyboard,
+} from "react-native";
+
+import { Button } from "../components/Button";
+import { SkillCard } from "../components/SkillCard";
 
 interface SkillData {
-    id: string;
-    name: string;    
+  id: string;
+  name: string;
 }
 
-export function Home(){
-  const [newSkill, setNewSkill] = useState('');
-  const [mySkills, setMySkills] = useState<SkillData[]>([]);  
-  const [gretting, setGretting] = useState('');
+export function Home() {
+  const [newSkill, setNewSkill] = useState("");
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
+  const [gretting, setGretting] = useState("");
 
-  
-  function handleAddNewSkill(){    
+  function handleAddNewSkill() {
     const data = {
-        id: String(new Date().getTime()),        
-        name: newSkill
-    }        
-    
-    Keyboard.dismiss()
+      id: String(new Date().getTime()),
+      name: newSkill,
+    };
 
-    setMySkills(oldState => [...oldState, data]);
+    Keyboard.dismiss();
+
+    setMySkills((oldState) => [...oldState, data]);
   }
 
-  function handleRemoveSkill(id: string){ 
-      setMySkills(oldState => oldState.filter(
-          skill => skill.id !== id
-      ));
+  function handleRemoveSkill(id: string) {
+    setMySkills((oldState) => oldState.filter((skill) => skill.id !== id));
   }
-
 
   useEffect(() => {
-      const currentHour = new Date().getHours();      
-      
-      if(currentHour < 12){
-          setGretting('Good morning');
-      }
-      else if(currentHour >= 12 && currentHour < 18){
-          setGretting('Good afternoon');
-      }else{
-          setGretting('Good night');
-      }
-      
-  }, [])
+    const currentHour = new Date().getHours();
 
-  return (    
-      <View style={styles.container} testID="home">       
-        
+    if (currentHour < 12) {
+      setGretting("Good morning");
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGretting("Good afternoon");
+    } else {
+      setGretting("Good night");
+    }
+  }, []);
 
-        <Text style={styles.title}>
-            Welcome, Daniel 
-        </Text>  
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
-        <Text style={styles.greetings}>
-            { gretting }       
-        </Text>   
+  return (
+    <View style={styles.container} testID="home">
+      <Text style={styles.title}>Welcome</Text>
 
-        <TextInput 
-            style={styles.input}
-            placeholder="New skill"
-            placeholderTextColor="#555"
-            onChangeText={setNewSkill}
-            testID="input-new"
-        />
+      <Text style={styles.greetings}>{gretting}</Text>
 
-        <Button 
-            testID="button-add"
-            title="Add"            
-            onPress={handleAddNewSkill}             
-        />
+      <TextInput
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#555"
+        onChangeText={setNewSkill}
+        testID="input-new"
+      />
 
-        <Text style={[styles.title, { marginVertical: 50}]}> 
-            MySkills
-        </Text>             
-        {mySkills && (
-            <FlatList 
-            testID="flatlist"
-                data={mySkills}
-                keyExtractor={item => item.id}
-                keyboardShouldPersistTaps="never"
-                renderItem={({ item }) => (
-                    <SkillCard 
-                        skill={item.name} 
-                        onPress={() => handleRemoveSkill(item.id)}
-                    />
-                )}
+      <Button testID="button-add" title="Add" onPress={handleAddNewSkill} />
+
+      <Text style={[styles.title, { marginVertical: 50 }]}>MySkills</Text>
+      {mySkills && (
+        <FlatList
+          testID="flatlist"
+          data={mySkills}
+          keyExtractor={(item) => item.id}
+          keyboardShouldPersistTaps="never"
+          renderItem={({ item }) => (
+            <SkillCard
+              skill={item.name}
+              onPress={() => handleRemoveSkill(item.id)}
             />
-        )}
-  
-            
-      </View>
-  )
+          )}
+        />
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,         
-        backgroundColor: '#121015',        
-        paddingVertical: 70,
-        paddingHorizontal: 30
-    },
-    title: {
-        color: '#FFF',
-        fontSize: 24,
-        fontWeight: 'bold'
-    },
-    input: {
-        backgroundColor: '#1F1E25',
-        color: '#FFF',
-        fontSize: 18,
-        padding: Platform.OS === 'ios' ? 15 : 10,
-        marginTop: 30,
-        borderRadius: 7
-    },    
-    greetings: {
-        color: '#FFF'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#121015",
+    paddingVertical: 70,
+    paddingHorizontal: 30,
+  },
+  title: {
+    color: "#FFF",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#1F1E25",
+    color: "#FFF",
+    fontSize: 18,
+    padding: Platform.OS === "ios" ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  greetings: {
+    color: "#FFF",
+  },
 });
